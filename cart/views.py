d4cbb2f136ca0699d0831880edefe5c5cdac35b8
Sarthak Kumar <ksarthak4ever@gmail.com>
2019-03-21 20:11:25 +0530

    added remove and delete product functionality to cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from shop.models import Product
 from . models import Cart,CartItem
 from django.core.exceptions import ObjectDoesNotExist
@@ -48,4 +48,22 @@ def cart_detail(request, total=0, counter=0, cart_items = None): #View for cart 
 	return render(request, 'cart.html', dict(cart_items = cart_items, total	= total, counter = counter)) 
 
 
+def cart_remove(request, product_id): #to remove a quantity product from cart
+	cart = Cart.objects.get(cart_id = _cart_id(request))
+	product = get_object_or_404(Product, id = product_id)
+	cart_item = CartItem.objects.get(product = product, cart = cart)
+	if cart_item.quantity > 1:
+		cart_item.quantity -= 1
+		cart_item.save()
+	else:
+		cart_item.delete()
+	return redirect('cart:cart_detail')
+
+
+def full_remove(request, product_id): #To remove all quantity of the product from cart
+	cart = Cart.objects.get(cart_id = _cart_id(request))
+	product = get_object_or_404(Product, id=product_id)
+	cart_item = CartItem.objects.get(product = product, cart = cart)
+	cart_item.delete()
+	return redirect('cart:cart_detail')
 
